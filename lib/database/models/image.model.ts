3218a0ade9c034bab479efbac802bd9171fc8a10
Nussy Rabinovich <Nussy@starsnacks.net>
2024-03-19 14:37:28 -0400

    update env

--- lib/database/models/image.model.ts
+++ lib/database/models/image.model.ts
@@ -4,12 +4,12 @@ export interface IImage extends Document {
    title: string;
    transformationType: string;
    publicId: string;
-   secureUrl: string;
+   secureURL: string;
    width?: number;
    height?: number;
    config?: object;
    transformationUrl?: string;
-   aspectRation?: string;
+   aspectRatio?: string;
    color?: string;
    prompt?: string;
    author: {
@@ -25,15 +25,15 @@ const ImageSchema = new Schema({
    title: { type: String, required: true },
    transformationType: { type: String, required: true },
    publicId: { type: String, required: true },
-   secureUrl: { type: URL, required: true },
+   secureURL: { type: String, required: true },
    width: { type: Number },
    height: { type: Number },
    config: { type: Object },
-   transformationUrl: { type: URL },
-   aspectRation: { type: String },
+   transformationUrl: { type: String },
+   aspectRatio: { type: String },
    color: { type: String },
    prompt: { type: String },
-   author: { type: Schema.Types.ObjectId, ref: "User" },
+   author: { type: Schema.Types.ObjectId, ref: 'User' },
    createdAt: { type: Date, default: Date.now },
    updatedAt: { type: Date, default: Date.now }
 });
